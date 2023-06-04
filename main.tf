8d6bc0002abbf04d07f6da5a98b1fc23ebee40f5
ekanandam14 <135482465+ekanandam14@users.noreply.github.com>
2023-06-04 15:58:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 # terraform code
+provider "aws" {
+  region = "us-west-2"  # Replace with your desired AWS region
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0123456789abcdef0"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"  # Replace with your desired instance type
+
+  tags = {
+    Name = "example-instance"
+  }
+}
