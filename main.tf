24ce5d1cbff424db8d9d339a2eb5263f3957cc7f
ekanandam14 <135482465+ekanandam14@users.noreply.github.com>
2023-06-04 10:25:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,1 @@
 # terraform code
-provider "aws" {
-  region = "us-west-2"  # Replace with your desired AWS region
-  access_key = "AKIAUJKYH5ZJV2SUMPWY"
-  secret_key = "vsPk3OxQ5QSoFHaxfc9Fhecn99rTRxEFrZpUPkEZ"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0123456789abcdef0"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"  # Replace with your desired instance type
-
-  tags = {
-    Name = "example-instance"
-  }
-}
